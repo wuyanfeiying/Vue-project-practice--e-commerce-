ee322d8fd4f90bb3ccbb686a52363c7d0796ec93
wuyanfeiying <wuyanfeiying@163.com>
2018-11-23 17:35:34 +0800

    2018-11-23 17:30:36 完成了首页网络数据获取 跳转到详情页 详情页数据获取 watch观察url改变 更新页面数据 UI框架Iview Element-ui 实现了小功能--返回顶部,图钉,分页

--- src/main.js
+++ src/main.js
@@ -2,13 +2,31 @@ import Vue from 'vue'
 import App from './App.vue'
 // 全局样式
 import './assets/site/css/style.css'
+
 // 导入每一页的组件
 import index from './components/index.vue'
+import detail from './components/detail.vue'
 
+// 导入moment插件
+import moment from 'moment'
 // 导入路由
 import VueRouter from 'vue-router'
+//导入 axios
+import axios from 'axios'
+//导入iview UI框架
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+Vue.use(iView);
+// 导入element-ui,轮播图插件
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+//设置到原型上的属性Vue中,建议使用$作为前缀,用来区分普通的属性
+Vue.prototype.$axios = axios;
+
 // 在模块化的开发环境下
-Vue.use(VueRouter) 
+Vue.use(VueRouter) ;
 
 Vue.config.productionTip = false
 
@@ -16,14 +34,23 @@ Vue.config.productionTip = false
 let routes  = [
   // 重定向到首页 /index
   {path:'/',redirect:'/index'},
-  {path:'/index',component:index}
+  {path:'/index',component:index},
+  // 使用动态路由匹配 传递参数
+  {path:'/detail/:artID',component:detail},
 ]
 
 
 // 实例化路由对象
 let router = new VueRouter({
   routes
-})
+});
+
+        // 注册全局过滤器
+        Vue.filter('handleTime',(value)=> {
+          var time = moment(value).format('YYYY年MM月DD日'); // 使用第三方插件,截取日期
+          return time;
+          }
+      );
 
 new Vue({
   render: h => h(App),
